153dce7b9beae0476eec4aaa9aff4e1d6a18432e
王广开 <416266475@qq.com>
2018-10-08 14:39:07 +0800

    添加字体文件

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import maincss from './assets/css/main.scss'
+import maincss from './assets/css/main.scss';
+import './assets/font/iconfont.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="c-red">
-        hello word
+      <div className="c-red iconfont icon-rili">
+        哈哈哈哈或
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');//优化编译信息展示
+const notifier = require('node-notifier');//优化错误信息展示
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.resolve(__dirname, './index.html'),
   filename: 'index.html',
@@ -26,8 +27,15 @@ const webpackCfg = {
       {
         test: /\.(sass|scss)$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff)$/,
+        loader: 'url-loader'
+      }]
   },
   devServer: {
     port: 9999,
@@ -42,7 +50,17 @@ webpackCfg.plugins.push(new FriendlyErrorsPlugin({
     messages: [`Your application is running here: http://localhost:${webpackCfg.devServer.port}`],
   },
   onErrors: function (severity, errors) {
-    console.log('errors:',errors);
+    if (severity !== 'error') {
+      return;
+    }
+    const error = errors[0];
+    console.log(error);
+    notifier.notify({
+      title: "Webpack error",
+      message: severity + ': ' + error.webpackError,
+      subtitle: error.file || '',
+      //icon: ICON
+    });
   },
 }));
 module.exports = webpackCfg;
