82f976a081e23a9129af07cce479c2c475cfa8f5
王广开 <41666475@qq.com>
2019-03-04 11:19:22 +0800

    Squashed 'gitcomon/' content from commit 3ca0d08
    git-subtree-dir: gitcomon
    git-subtree-split: 3ca0d0822f001ab8d28ad3d90a59967b3a50b29a

---
+++ component/Grid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Grid extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+        <tr>
+          <th>姓名</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>wang</td>
+        </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
